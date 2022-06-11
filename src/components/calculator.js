4cc2c0b475119ae5eacf867f8794ac16a4548d8f
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-11 05:57:40 +0100

    Add linters error correction

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from './Button';
+import Button from '../components/Button'
 import calculate from '../logic/calculate';
 import DisplayResult from './Display';
 
