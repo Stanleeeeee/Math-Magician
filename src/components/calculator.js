f07413205a1588787181cae73bedbcb78595949b
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-11 17:26:08 +0100

    Add linters corrections

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React, { useState } from 'react';
-import Button from '../components/Button.js';
+import Button from './Button';
 import calculate from '../logic/calculate';
 import DisplayResult from './Display';
 
