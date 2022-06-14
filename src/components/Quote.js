72d2b02faea7416da920c94b870954ccb2373fa7
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-14 16:54:12 +0100

    Add Quote to the calculator app

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div>
+      <h3 className="quoteHeader homeText ">
+        Mathematics is not about numbers,equation,computations,or algorithms :
+        It is about understanding.-William Paul Thurston
+      </h3>
+    </div>
+  );
+}
+export default Quote;
