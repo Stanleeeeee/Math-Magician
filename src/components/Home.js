851fe762ad5c42e0e2c50b47b714b8311f2ca79f
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-14 21:27:42 +0100

    Add update linters corrections

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="homePage">
       <h2 className="homeText">Welcome to our page!</h2>
-      <p className="homeText">
+        <p className="homeText">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan
             felis libero, ut molestie erat cursus sit amet. Nam ac mauris pulvinar,
             consectetur lectus vitae, ultrices justo. Donec ultricies arcu in nunc
@@ -15,7 +15,7 @@ function Home() {
             velit eget quam molestie, pharetra molestie dolor pulvinar. Ut commodo,
             sem at tristique facilisis, nisl elit tincidunt ligula, sed molestie
             diam tellus fringilla est.
-      </p>
+        </p>
     </div>
   );
 }
