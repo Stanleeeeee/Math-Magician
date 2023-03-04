6c1bb9e6092b08d2f6c3dd66f999f382baed05a9
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-03-04 12:41:21 +0100

    Add image to page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles/Nav.css';
 
 function Navbar() {
   return (
-    <nav className="navClass">
-      <h1 className="pageTitle">Math Magicians</h1>
-
-      <Link to="/" className="try">
-        Home
-      </Link>
-      <Link to="/calculator" className="try">
-        Calculator
-      </Link>
-      <Link to="/quote" className="try borderRemove">
-        Quote
-      </Link>
+    <nav className='navBar'>
+      <h1 className='title'>Math Magicians</h1>
+      <ul className='linksContainer'>
+        <li className='linkItems'>
+          <Link to='/' className='links'>
+            Home
+          </Link>
+        </li>
+        <div className='separateDiv' />
+        <li className='linkItems'>
+          <Link to='/calculator' className='links'>
+            Calculator
+          </Link>
+        </li>
+        <div className='separateDiv' />
+        <li className='linkItems'>
+          <Link to='/quote' className='links'>
+            Quote
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
