f7a6bd0e77d400789826947bfb5eb9795a975f62
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-03-04 12:43:44 +0100

    Rearrage text, add image and add button for array of quotes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles/QuotesPage.css';
+import quotes from './arrayOfQuotes';
 
 function Quote() {
+  const [quote, setQuote] = useState('');
+
+  const updateQuote = () => {
+    const quoteIndex = Math.round(Math.random() * (quotes.length - 1));
+    setQuote(quotes[quoteIndex]);
+  };
+
   return (
-    <div>
-      <h3 className="quoteHeader homeText ">
-        Mathematics is not about numbers,equation,computations,or algorithms :
-        It is about understanding.-William Paul Thurston
-      </h3>
+    <div className="quoteContainer">
+      <p className="quotes">
+        &quot;
+        {quote === '' ? 'Math is the source code of reality' : quote}
+        &quot;
+      </p>
+      <button type="button" onClick={updateQuote} className="generateQuotes">
+        Generate quote
+      </button>
     </div>
   );
-}
+};
+
 export default Quote;
